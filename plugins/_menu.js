c149e0ea7f5b8451f536916b143545185463c2ee
BRUTOO-6667777 <tfdfghffvcf@gmail.com>
2024-12-29 16:56:08 +0530

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -90,7 +90,7 @@ bot.addCommand(
       }
     })
     const [date, time] = getDate()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
+    let msg = `\`\`\`╭═══ BRUTOZZ ═══⊷
 ┃❃╭──────────────
 ┃❃│ Prefix : ${ctx.PREFIX}
 ┃❃│ User : ${message.pushName}
